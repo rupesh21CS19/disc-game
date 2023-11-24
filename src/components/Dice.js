05260393e880daaf00d9937f5515be8f35ea782a
rupesh21CS19 <nikku090703@gmail.com>
2023-11-25 00:22:58 +0530

    add

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,9 +1,10 @@
 const Dice = ({ diceNumber, setDiceNumber, RandomNumber }) => {
+  console.log(diceNumber);
   return (
     <div className="m-auto pt-4  w-max text-center font-semibold text-lg cursor-pointer">
       <img
         className="bg-white w-40"
-        src={`../res/dice_${diceNumber}.png`}
+        src="../res/dice_1.png"
         alt=""
         onClick={RandomNumber}
       />
